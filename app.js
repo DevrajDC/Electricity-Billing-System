7c6940570e6ea0f315816a38ce00795fde41e6f9
Pranath Naik <pranathnaik12@gmail.com>
2022-09-06 12:50:15 +0530

    first commit

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 //hide div with id sub-menu-1 when click on button
 
-var toggleitem=1;
+var toggleitem=0;
 function showhide(num)
 {
-    console.log(num);
-    if(toggleitem==1){
- document.getElementById("sub-menu-"+num).style.display = "none";
+    if(toggleitem==0){
+    document.getElementById("sub-menu-"+num).style.display = "none";
     toggleitem=0;
-}
+    }
 else{
+    console.log("show");
  document.getElementById("sub-menu-"+num).style.display = "block";
  toggleitem=1;
-}}
+}
+}
--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 //hide div with id sub-menu-1 when click on button
-
 var toggleitem=1;
 function showhide(num)
 {
-    console.log(num);
-    if(toggleitem==1){
- document.getElementById("sub-menu-"+num).style.display = "none";
+    if(toggleitem==0){
+    document.getElementById("sub-menu-"+num).style.display = "none";
+    toggleitem=1;
+    }
+    else{
+    document.getElementById("sub-menu-"+num).style.display = "block";
     toggleitem=0;
+    }
 }
-else{
- document.getElementById("sub-menu-"+num).style.display = "block";
- toggleitem=1;
-}}
