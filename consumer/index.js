3c3d1d072c73988cc0e3702ee431932f4e3d9f5e
akashsolienkar <akashsolienkar@gmail.com>
2022-09-07 15:48:50 +0530

    admin counsumer page

--- consumer/index.js
+++ consumer/index.js
@@ -11,3 +11,15 @@ function showhide(num)
     toggleitem=0;
     }
 }
+
+function changeslide(pagename1,pagemame2,pagename3)
+{
+    document.getElementById(pagename1).style.display = "block";
+    document.getElementById(pagemame2).style.display = "none";
+    document.getElementById(pagename3).style.display = "none";
+}
+function changename(p1,p2)
+{
+    document.getElementById('hh1').innerHTML=p1
+    document.getElementById('hh1').innerHTML=p1
+}
