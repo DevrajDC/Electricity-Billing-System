91cbdcdef2e1eeaa8b223eae227738ed4b9db565
Pranath Naik <pranathnaik12@gmail.com>
2022-09-07 14:56:32 +0530

    integrated admin modal

--- consumer/index.js
+++ consumer/index.js
@@ -1,14 +1,11 @@
 //hide div with id sub-menu-1 when click on button
-var toggleitem=1;
-function showhide(num)
-{
-    if(toggleitem==0){
-    document.getElementById("sub-menu-"+num).style.display = "none";
-    toggleitem=1;
-    }
-    else{
-    document.getElementById("sub-menu-"+num).style.display = "block";
-    toggleitem=0;
-    }
+var toggleitem = 1;
+function showhide(num) {
+  if (toggleitem == 0) {
+    document.getElementById("sub-menu-" + num).style.display = "none";
+    toggleitem = 1;
+  } else {
+    document.getElementById("sub-menu-" + num).style.display = "block";
+    toggleitem = 0;
+  }
 }
-
