98264c3cf258a32de955be3696deb5f225c59ef4
Devraj Chatribin <devrajchatribin9978@gmail.com>
2022-09-13 10:37:47 +0530

    removed unneccessary files

--- modal/index.js
+++
@@ -1,7 +0,0 @@
-
-function toggleModal(modalID){
-    document.getElementById(modalID).classList.toggle("hidden");
-    document.getElementById(modalID + "-backdrop").classList.toggle("hidden");
-    document.getElementById(modalID).classList.toggle("flex");
-    document.getElementById(modalID + "-backdrop").classList.toggle("flex");
-  }
