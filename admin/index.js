886b9d0ac387d7f743597eb66f7cfabd18b760ce
akashsolienkar <akashsolienkar@gmail.com>
2022-09-13 12:07:07 +0530

    login done

--- admin/index.js
+++ admin/index.js
@@ -4,7 +4,6 @@ function toggleModal(modalID) {
   document.getElementById(modalID).classList.toggle("flex");
   document.getElementById(modalID + "-backdrop").classList.toggle("flex");
 }
-
 //hide div with id sub-menu-1 when click on button
 var toggleitem = 1;
 function showhide(num) {
@@ -51,3 +50,87 @@ function navcolor(j1, j2, j3,style1,style2) {
 var i = 0;
 var imagesbg = Array("url(../assets/Electricity.jpg)", "url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)");
 setInterval(() => { console.log(document.getElementById('img').style.backgroundImage); document.getElementById('img').style.backgroundImage = imagesbg[i]; i++; if(i==2){i=0;} }, 3000);
+
+// validation
+
+const form = document.getElementById('f1');
+const email = document.getElementById('email2');
+const password = document.getElementById('password2');
+
+//Show input error messages
+function showError(input, message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+}
+
+//show success colour
+function showSucces(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+}
+
+//check email is valid
+function checkEmail(input) {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(re.test(input.value.trim())) {
+        showSucces(input)
+    }else {
+        showError(input,'Email is not invalid');
+    }
+}
+
+//checkRequired fields
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input){
+        if(input.value.trim() === ''){
+            showError(input,`${getFieldName(input)} is required`)
+        }else {
+            showSucces(input);
+        }
+    });
+}
+
+//check input Length
+function checkLength(input, min ,max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);
+    }else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be les than ${max} characters`);
+    }else {
+        showSucces(input);
+    }
+}
+
+//get FieldName
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// // check passwords match
+// function checkPasswordMatch(input1, input2) {
+//     if(input1.value !== input2.value) {
+//         showError(input2, 'Passwords do not match');
+//     }
+// }
+
+//Event Listeners
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+
+    checkRequired([email, password]);
+    checkLength(password,6,25);
+    checkEmail(email);
+}); 
+
+const form2 = document.getElementById('l2');
+form2.addEventListener('submit',function(e) {
+  e.preventDefault();
+
+  checkRequired([document.getElementById('password1'), document.getElementById('email1')]);
+  checkLength(document.getElementById('password1'),6,25);
+  checkLength(document.getElementById('phone'),10,10);
+  checkEmail(document.getElementById('email1'));
+}); 
+
--- consumer/login.php
+++ consumer/login.php
@@ -9,7 +9,6 @@
     <link rel="stylesheet" href="styler.css" type="text/css">
     <title>Document</title>
 </head>
-
 <body>
     <div class="bg-white">
         <div class="flex justify-center h-screen">
@@ -25,7 +24,7 @@
                 </div>
             </div>
             <!-- nav -->
-            <div class="max-w-md px-6 mx-auto lg:mt-64 sm:mt-2">
+            <div class="max-w-md px-6 mx-auto lg:mt-20 sm:mt-2">
                 <!-- enter nav -->
                 <div class="">
                     <h2 class=" mt-4 font-bold text-center text-indigo-600 text-4xl mb-8">Oneshield EBS</h2>
@@ -41,36 +40,37 @@
                     </nav>
                     <!-- sign in -->
                     <div id="l1" class="mt-8">
-                        <form id="l1" action="#" method="POST" class="space-y-6 pr">
-                            <div style="opacity:0; animation-name:changeform; animation-delay:0s; animation-duration: 1s;animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                        <form id="f1" action="#" method="POST" class="space-y-6 pr">
+                            <div style="opacity:0; animation-name:changeform; animation-delay:0s; animation-duration: 0.5s;animation-timing-function:ease-in;animation-fill-mode: forwards;">
                                 <label for="email" class="block text-sm font-medium text-gray-700"> Email address
                                 </label>
                                 <div class="mt-1">
-                                    <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    <input id="email2" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
                                 </div>
                             </div>
 
-                            <div class="space-y-1" style="opacity:0; animation-name:changeform; animation-delay:0.03s;  animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                            <div class="space-y-1" style="opacity:0; animation-name:changeform; animation-delay:0.03s;  animation-duration: 0.5; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                                 <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
                                 <div class="mt-1">
-                                    <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                    <input id="password2" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <small></small>
                                 </div>
                             </div>
 
-                            <div class="flex items-center justify-between" style="opacity:0; animation-name:changeform; animation-delay:0.06s;  animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                            <div class="flex items-center justify-between" style="opacity:0; animation-name:changeform; animation-delay:0.06s;  animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                                 <div class="flex items-center">
                                     <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
                                     <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me
                                     </label>
                                 </div>
 
-                                <div class="text-sm" style="opacity:0; animation-name:changeform; animation-delay:0.09s; animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode:forwards;">
+                                <div class="text-sm" style="opacity:0; animation-name:changeform; animation-delay:0.09s; animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode:forwards;">
                                     <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500"> Forgot your
                                         password? </a>
                                 </div>
                             </div>
 
-                            <div style="opacity:0;animation-name:changeform; animation-delay:0.12s; animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                            <div style="opacity:0;animation-name:changeform; animation-delay:0.12s; animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                                 <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Sign
                                     in</button>
                             </div>
@@ -81,34 +81,38 @@
 
                     <!-- register -->
                     <form id="l2" style="display:none;" action="#" method="POST" class="space-y-4 pr mt-8">
-                        <div style="opacity:0; animation-name:changeform; animation-delay:0s; animation-duration: 1s;animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                        <div style="opacity:0; animation-name:changeform; animation-delay:0s; animation-duration: 0.5s;animation-timing-function:ease-in;animation-fill-mode: forwards;">
                             <label for="email" class="block text-sm font-medium text-gray-700"> Email address
                             </label>
                             <div class="mt-1">
-                                <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <input id="email1" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <small></small>
                             </div>
                         </div>
-                        <div style=" opacity:0; animation-name:changeform; animation-delay:0.03s; animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                        <div style=" opacity:0; animation-name:changeform; animation-delay:0.03s; animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                             <label for="email" class="block text-sm font-medium text-gray-700"> Full Name </label>
                             <div class="mt-1">
-                                <input id="email" name="email" type="email" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <input id="name" name="email" type="text" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <small></small>
                             </div>
                         </div>
 
-                        <div style="opacity:0; animation-name:changeform; animation-delay:0.06s; animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                        <div style="opacity:0; animation-name:changeform; animation-delay:0.06s; animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                             <label for="email" class="block text-sm font-medium text-gray-700"> Phone </label>
                             <div class="mt-1">
-                                <input id="email" name="email" type="number" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <input id="phone" name="email" type="number" autocomplete="email" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <small></small>
                             </div>
                         </div>
 
-                        <div class="space-y-1" style="opacity:0; animation-name:changeform; animation-delay:0.12s; animation-duration: 1s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                        <div class="space-y-1" style="opacity:0; animation-name:changeform; animation-delay:0.12s; animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                             <label for="password" class="block text-sm font-medium text-gray-700"> Password </label>
                             <div class="mt-1">
-                                <input id="password" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                                <input id="password1" name="password" type="password" autocomplete="current-password" required class="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm">
+                            <small></small>
                             </div>
                         </div>
-                        <div style="opacity:0; animation-name:changeform; animation-delay:0.15s; animation-duration: 5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
+                        <div style="opacity:0; animation-name:changeform; animation-delay:0.15s; animation-duration: 0.5s; animation-timing-function:ease-in;animation-fill-mode: forwards;">
                             <button type="submit" class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Register</button>
                         </div>
                     </form>
