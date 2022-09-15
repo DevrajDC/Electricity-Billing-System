b0325420cffa6a1067a6a79b6149af5a2b2b2300
Devraj Chatribin <devrajchatribin9978@gmail.com>
2022-09-15 15:34:16 +0530

    updated connections page

--- admin/connections.php
+++ admin/connections.php
@@ -1,6 +1,6 @@
-<?php 
-  include("includes/navbar.php"); 
-  include("includes/dbConnection.php");   
+<?php
+include("includes/navbar.php");
+include("includes/dbConnection.php");
 ?>
 <div class="md:pl-64 flex flex-col flex-1">
   <div class="sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100">
@@ -46,42 +46,42 @@
                   </thead>
                   <tbody class="bg-white divide-y divide-gray-200">
                     <?php
-                      $quer = "SELECT * FROM Users";
-                      $result = $conn->query($quer);
-                      while($row = $result->fetch_assoc()) {
+                    $quer = "SELECT * FROM Users WHERE consumer_id IN (SELECT consumer_id FROM meterdata)";
+                    $result = $conn->query($quer);
+                    while ($row = $result->fetch_assoc()) {
                     ?>
-                    <tr>
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        <?php echo $row["consumer_id"]; ?>
-                      </td>
-                      <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
-                        <?php echo $row["name"]; ?>
-                      </td>
-                      <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                        <?php echo $row["email"]; ?>
-                      </td>
-                      <td class="px-6 py-4 flex gap-12 whitespace-nowrap text-left">
-                        <?php
+                      <tr>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                          <?php echo $row["consumer_id"]; ?>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                          <?php echo $row["name"]; ?>
+                        </td>
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                          <?php echo $row["email"]; ?>
+                        </td>
+                        <td class="px-6 py-4 flex gap-12 whitespace-nowrap text-left">
+                          <?php
                           $id = $row["consumer_id"];
                           $qr = "SELECT meter_num FROM meterdata WHERE consumer_id = $id";
                           $res = $conn->query($qr);
                           $num_meters = mysqli_num_rows($res);
                           $i = 1;
-                          while($r = $res->fetch_assoc()) {  
-                        ?>  
-                        <a href="consumerdetails.php?meter_num=<?php echo $r["meter_num"]; ?>&consumer_name=<?php echo $row["name"]; ?>&consumer_id=<?php echo $row["consumer_id"]; ?>&bill=none" class="text-indigo-600 text-sm font-bold">
-                          Meter <?php echo $i; ?>
-                        </a>
-                        <?php
+                          while ($r = $res->fetch_assoc()) {
+                          ?>
+                            <a href="consumerdetails.php?meter_num=<?php echo $r["meter_num"]; ?>&consumer_name=<?php echo $row["name"]; ?>&consumer_id=<?php echo $row["consumer_id"]; ?>&bill=none" class="text-indigo-600 text-sm font-bold">
+                              Meter <?php echo $i; ?>
+                            </a>
+                          <?php
                             $i++;
                           }
-                        ?>
-                      </td>
-                    </tr>
+                          ?>
+                        </td>
+                      </tr>
                     <?php
-                      }
+                    }
                     ?>
-                  
+
               </div>
             </div>
   </main>
